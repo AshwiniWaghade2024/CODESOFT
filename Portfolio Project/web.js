0fe4aa20e07c7494628ac68bc312d59f556117d1
AshwiniWaghade2024 <158551421+AshwiniWaghade2024@users.noreply.github.com>
2024-02-11 21:45:28 +0530

    Add files via upload

---
+++ Portfolio Project/web.js
@@ -0,0 +1,15 @@
+document.getElementById('skills').addEventListener('click', function() {
+    document.querySelector('#container2').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.getElementById('projects').addEventListener('click', function() {
+    document.querySelector('#container3').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.getElementById('resume').addEventListener('click', function() {
+    document.querySelector('#container4').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.getElementById('contacts').addEventListener('click', function() {
+    document.querySelector('#container5').scrollIntoView({ behavior: 'smooth' });
+  });
